bc5be32c5424657944a158f539bfa2477d793f30
Chenming <852098505@qq.com>
2021-12-31 11:39:59 +0800

    MycatConfig

--- ObjectEqualsAndHashcode/src/main/java/com/cm/mainTest.java
+++ ObjectEqualsAndHashcode/src/main/java/com/cm/mainTest.java
@@ -4,6 +4,8 @@ import com.cm.domian.User;
 import com.cm.domian.UserLombok;
 import lombok.extern.slf4j.Slf4j;
 
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
 import java.util.Date;
 
 @Slf4j
@@ -11,28 +13,38 @@ public class mainTest {
 
     public static void main(String[] args) {
 
-        User user1 = new User();
-        user1.setUsername("chenmign");
-        user1.setDate(new Date());
-        user1.setAge(123);
+//        User user1 = new User();
+//        user1.setUsername("chenmign");
+//        user1.setDate(new Date());
+//        user1.setAge(123);
+//
+//        User user2 = new User();
+//        user2.setUsername("chenmign");
+//        user2.setAge(123);
+//
+//        log.info("手写equals和hashcode  " + user1.equals(user2));
+//
+//        UserLombok user3 = new UserLombok();
+//        user3.setUsername("chenmign");
+//        user3.setDate(new Date());
+//        user3.setAge(123);
+//
+//        UserLombok user4 = new UserLombok();
+//        user4.setUsername("chenmign");
+//        user4.setAge(123);
+//
+//        log.info("使用lombok + exclude排除比较属性  " + user3.equals(user4));
+        Timestamp timestamp = new Timestamp(1640851813000L);
+        Timestamp timestamp1 = new Timestamp(1640851813000L-60000L);
 
-        User user2 = new User();
-        user2.setUsername("chenmign");
-        user2.setAge(123);
+        String format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(timestamp);
+        String format1 = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss").format(timestamp1);
 
-        log.info("手写equals和hashcode  " + user1.equals(user2));
+        log.info(format);
 
-        UserLombok user3 = new UserLombok();
-        user3.setUsername("chenmign");
-        user3.setDate(new Date());
-        user3.setAge(123);
-
-        UserLombok user4 = new UserLombok();
-        user4.setUsername("chenmign");
-        user4.setAge(123);
-
-        log.info("使用lombok + exclude排除比较属性  " + user3.equals(user4));
 
     }
+
+
 
 }
