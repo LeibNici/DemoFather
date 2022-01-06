1f00da7d4fd9c23b00a46ef68899a15de7a587ba
Chenming <852098505@qq.com>
2022-01-06 20:32:02 +0800

    修改application.yml中sharding下table下的表名

--- ShardingSphere/src/main/java/com/shardingSphere/utils/TableUtil.java
+++ ShardingSphere/src/main/java/com/shardingSphere/utils/TableUtil.java
@@ -32,14 +32,21 @@ public class TableUtil {
     }
 
     public static String createTable(String date) {
-        String tableName = "user_" + date;
+        String tableName = "bus_car_operation_" + date;
         String sql = "CREATE TABLE " + tableName + " (\n" +
-                "  `id` bigint NOT NULL AUTO_INCREMENT,\n" +
-                "  `name` varchar(255) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL,\n" +
-                "  `create_time` datetime DEFAULT NULL,\n" +
-                "  `age` int DEFAULT NULL,\n" +
-                "  PRIMARY KEY (`id`) USING BTREE\n" +
-                ") ENGINE=InnoDB AUTO_INCREMENT=1022570499 DEFAULT CHARSET=utf8mb3";
+                "  `id` bigint NOT NULL AUTO_INCREMENT COMMENT '主键',\n" +
+                "  `car_num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '车牌号',\n" +
+                "  `site_x` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '地点x坐标',\n" +
+                "  `site_y` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '地点y坐标',\n" +
+                "  `area_num` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '区域编号',\n" +
+                "  `record_date` datetime DEFAULT NULL COMMENT '记录时间',\n" +
+                "  `area_location` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '所在区域位置',\n" +
+                "  `card_number` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_bin DEFAULT NULL COMMENT '车辆定位卡号',\n" +
+                "  PRIMARY KEY (`id`) USING BTREE,\n" +
+                "  UNIQUE KEY `id` (`id`) USING BTREE,\n" +
+                "  KEY `car_date_x_y_cardNum` (`car_num`,`record_date`,`card_number`) USING BTREE,\n" +
+                "  KEY `x` (`car_num`,`site_x`) USING BTREE\n" +
+                ") ENGINE=InnoDB AUTO_INCREMENT=28974886 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin ROW_FORMAT=DYNAMIC COMMENT='车辆运行记录表'";
         tableUtil.tableCheckMapper.executeSql(sql);
         return tableName;
     }
