012d1ab7067775958c2bb80b24653138579b6297
Chenming <852098505@qq.com>
2022-03-03 20:43:15 +0800

    rabbitmq

---
+++ SpringMQ-P/src/main/java/com/SpringMQ/MQPApplication.java
@@ -0,0 +1,16 @@
+package com.SpringMQ;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author chenming
+ * @description
+ * @create: 2022-03-03
+ */
+@SpringBootApplication
+public class MQPApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MQPApplication.class);
+    }
+}
